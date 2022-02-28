0ab8e3ac84123e310188ab579f3c6da3550cb657
lsvih <lsvih@qq.com>
2022-02-28 11:26:57 +0800

    avoid inplace operate in the latest pytorch.

--- model.py
+++ model.py
@@ -25,7 +25,7 @@ class HLG(BertPreTrainedModel):
             input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask, output_all_encoded_layers=False)
         # Padding
         char_reps = F.pad(char_reps, [0, 0, 0, graphs[0].size(1) - char_reps.size(1)])
-
+        # MSIP
         w1 = self.c1_to_w1(torch.transpose(graphs[0], -1, -2), char_reps, char_mask)
         s1 = self.w1_to_s1(torch.transpose(graphs[1], -1, -2), w1, word_mask)
         w2_1 = self.s1_to_w2(graphs[1], s1, sentence_mask)
@@ -41,14 +41,13 @@ class GCNLayer(nn.Module):
     def __init__(self, hidden_size):
         super(GCNLayer, self).__init__()
         self.gcn = nn.Linear(hidden_size, hidden_size)
-        self.linear = nn.Linear(hidden_size, hidden_size)
         self.relu = nn.ReLU()
 
     def forward(self, adj, nodes_hidden, nodes_mask):
         # norm adj
         scale = adj.sum(dim=-1)
         scale[scale == 0] = 1
-        adj /= scale.unsqueeze(-1).repeat(1, 1, adj.shape[-1])
+        adj = adj / scale.unsqueeze(-1).repeat(1, 1, adj.shape[-1])
         nodes_hidden = nodes_hidden * nodes_mask.unsqueeze(-1)
         nodes_hidden = self.gcn(torch.matmul(adj, nodes_hidden))
         return self.relu(nodes_hidden)
